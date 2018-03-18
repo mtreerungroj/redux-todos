01fd86b5220b33f79ab53655f00bcc21f1d4f95f
mtreerungroj <m.treerungroj@gmail.com>
2018-03-18 10:34:52 +0700

    add visibilityFilter reducer for filter todos

--- src/actions/index.js
+++ src/actions/index.js
@@ -12,3 +12,14 @@ export const toggleTodo = id => ({
   type: 'TOGGLE_TODO',
   id
 })
+
+export const VisibilityFilter = {
+  SHOW_ALL: 'SHOW_ALL',
+  SHOW_COMPLETED: 'SHOW_COMPLETED',
+  SHOW_ACTIVE: 'SHOW_ACTIVE'
+}
+
+export const setVisibilityFilter = filter => ({
+  type: 'SET_VISIBILITY_FILTER',
+  filter: filter || VisibilityFilter.SHOW_ALL
+})
--- src/containers/VisibleTodoList.js
+++ src/containers/VisibleTodoList.js
@@ -2,7 +2,7 @@ import TodoList from '../components/TodoList'
 import { connect } from 'react-redux'
 import { toggleTodo } from '../actions/index'
 
-const mapStateToProps = state => ({ todos: state })
+const mapStateToProps = state => ({ todos: state.todos }) // ชื่อที่สั่งตอน combine reducer ไม่ใช่ชื่อ reducer จริงๆ
 const mapDispatchToProps = dispatch => ({
   toggleTodo: id => dispatch(toggleTodo(id))
 })
